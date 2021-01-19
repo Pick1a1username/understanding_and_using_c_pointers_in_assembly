c34690afd570eeb0ffc62538165f97585bce99a7
Pick1a1username <xscreensaver@outlook.com>
2021-01-19 21:50:15 +0900

    20210119_2

--- chapter6/single_linked_list/single_linked_list.asm
+++ chapter6/single_linked_list/single_linked_list.asm
@@ -1,4 +1,5 @@
 extern printf
+extern strcmp
 
 struc _employee
     _e_name:  resb 32
@@ -8,13 +9,25 @@ endstruc
 section .data
     samuel:
         istruc _employee
-            at _e_name, db  "Samuel"
+            at _e_name, db  "Samuel", 0
             at _e_age,  db  32
         iend
-    fmt_str  db  "%s", 10
+    sally:
+        istruc _employee
+            at _e_name, db  "Sally", 0
+            at _e_age,  db  28
+        iend
+    susan:
+        istruc _employee
+            at _e_name, db  "Susan", 0
+            at _e_age,  db  45
+        iend
+    fmt_str  db  "%s", 10, 0
+    fmt_int  db  "%d", 10, 0
 
 section .text
-    global main
+
+global main
 main:
     push rbp
     mov  rbp, rsp
@@ -23,5 +36,27 @@ main:
     lea  rsi, [samuel+_e_name]
     call printf
 
+    lea  rdi, [sally]
+    lea  rsi, [samuel]
+    call compare_employee
+
+    mov  rdi, fmt_int
+    mov  rsi, rax
+    call printf
+
     leave
     ret
+
+global compare_employee
+compare_employee:
+    section .text 
+        push rbp
+        mov  rbp, rsp
+
+        lea rdi, [rdi+_e_name]
+        lea rsi, [rsi+_e_name]
+        call strcmp
+
+        leave
+        ret
+  
