ec5b94deec6105fb9e371044934f4a139edfe436
Pick1a1username <xscreensaver@outlook.com>
2021-01-19 21:09:57 +0900

    20210119_0

--- chapter6/single_linked_list/single_linked_list.asm
+++ chapter6/single_linked_list/single_linked_list.asm
@@ -1,19 +1,19 @@
 extern printf
 
-struc mytype
-    mt_long:  resd 1
-    mt_word:  resw 1
-    mt_byte:  resb 1
-    mt_str:   resb 32
+struc _employee
+    .long:  resd 1
+    .word:  resw 1
+    .byte:  resb 32
+    .str:   resb 32
 endstruc
 
 section .data
     mystruc:
         istruc mytype
-            at mt_long, dd    123456
-            at mt_word, dw    1024
-            at mt_byte, db    "x"
-            at mt_str,  db    "hello, world", 13, 10, 0
+            at .long, dd    123456
+            at .word, dw    1024
+            at .byte, db    "x"
+            at .str,  db    "hello, world", 13, 10, 0
         iend
     fmt_str  db  "%s"
 
